657c11013d89b014288abe8155380f09e1d481b1
elfas <kibetelfas17@gmail.com>
2025-03-26 23:00:54 +0300

    js initial update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const base_url =
+    "https://my-json-server.typicode.com/ElfasKibet/Phase-1-project-Kitchen-items/apiProducts";
+  console.log("API base URL:", base_url);
+});
